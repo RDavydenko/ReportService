5c8d94066ec199006b7800941a6fe682f3096907
Роман Давыденко <dra@pkzdrav.ru>
2020-09-23 22:17:37 +0700

    Изменена структура проекта

---
+++ sources/ReportServiceAPI/sources/Configs/ReportMappingProfile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+using ReportServiceAPI.sources.DTOs;
+using ReportServiceAPI.sources.Models;
+
+namespace ReportServiceAPI.sources.Configs
+{
+	/// <summary>
+	/// Маппинг-профиль для класса <see cref="Report"/>
+	/// </summary>
+	public class ReportMappingProfile : Profile
+	{
+		public ReportMappingProfile()
+		{
+			// Из Report в ReportDTO
+			CreateMap<Report, ReportDTO>()
+				.ForMember(x => x.UserId, opt =>
+				{
+					// Если User не NULL, то записываем в поле UserId значение User.Id
+					opt.Condition(x => x.User != null);
+					opt.MapFrom(x => x.User.Id);
+				});
+
+			// Из ReportDTO в Report
+			CreateMap<ReportDTO, Report>()
+				// Создаем нового пользователя с полем Id из ReportDTO.UserId
+				.ForMember(x => x.User, opt => opt.MapFrom(x => new User { Id = x.UserId.GetValueOrDefault() }))
+				// Если в Date NULL, то берем системное время, иначе время, указанное в Date
+				.ForMember(x => x.Date, opt => opt.MapFrom((rDTO, r) => rDTO.Date.HasValue ? rDTO.Date.Value : DateTime.Now)); ;
+
+			// Из Report в IdDTO
+			CreateMap<Report, IdDTO>();
+		}
+	}
+}
---
+++ sources/ReportServiceAPI/sources/Configs/UserMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+using ReportServiceAPI.sources.DTOs;
+using ReportServiceAPI.sources.Models;
+
+namespace ReportServiceAPI.sources.Configs
+{
+	/// <summary>
+	/// Маппинг-профиль для класса <see cref="User"/>
+	/// </summary>
+	public class UserMappingProfile : Profile
+	{
+		public UserMappingProfile()
+		{
+			// Из User в UserDTO
+			CreateMap<User, UserDTO>();
+
+			// Из UserDTO в User
+			CreateMap<UserDTO, User>();
+
+			// Из User в IdDTO
+			CreateMap<User, IdDTO>();
+		}
+	}
+}
