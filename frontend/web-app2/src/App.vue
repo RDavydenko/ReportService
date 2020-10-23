8cd78249bf7fab0c8e03816304abe455bf537f58
Роман Давыденко <ishadows888@gmail.com>
2020-10-23 15:29:31 +0700

    Добавлено навигационное меню

--- frontend/web-app2/src/App.vue
+++ frontend/web-app2/src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header>
+        <el-menu class="el-menu-demo" mode="horizontal" default-active="/" :router="true">
+          <el-menu-item index="/" :route="{ name: 'Home' }">Home</el-menu-item>
+          <el-menu-item index="/users" :route="{ name: 'Users' }"
+            >Users</el-menu-item
+          >
+          <el-menu-item index="/reports" :route="{ name: 'Reports' }"
+            >Reports</el-menu-item
+          >
+        </el-menu>
+      </el-header>
+      
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
